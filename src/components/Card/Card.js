a31c2befa07e8ce45da9d6811a275741ee4722db
Vladyslav Tkachenko <vlad@wtf.lv>
2023-05-10 20:51:37 +0300

    adding pagination and accordion

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,31 +1,83 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Card.css";
 
+import ReactPaginate from "react-paginate";
+
 const Card = (props) => {
+  const items = props?.data;
+
+  function Items({ currentItems }) {
+    return (
+      <>
+        {currentItems?.map((card, index) => {
+          return (
+            <div className="card" key={index}>
+              <p className="date">
+                {card.date} {card.category}
+              </p>
+              <p className="content">
+                <a href={card?.link}>{card.title}</a>
+              </p>
+              <hr />
+
+              <div>
+                {card.linkedThemes?.map((item, i) => (
+                  <a href={item.link ? item.link : ""} className="link" key={i}>
+                    {item.name}
+                  </a>
+                ))}
+              </div>
+            </div>
+          );
+        })}
+      </>
+    );
+  }
+
+  const [itemOffset, setItemOffset] = useState(0);
+
+  // Simulate fetching items from another resources.
+  // (This could be items from props; or items loaded in a local state
+  // from an API endpoint with useEffect and useState)
+
+  const itemsPerPage = 9;
+  const endOffset = itemOffset + itemsPerPage;
+  const currentItems = items?.slice(itemOffset, endOffset);
+  const pageCount = Math.ceil(items?.length / itemsPerPage);
+
+  // Invoke when user click to request another page.
+  const handlePageClick = (event) => {
+    const newOffset = (event.selected * itemsPerPage) % items.length;
+    console.log(
+      `User requested page number ${event.selected}, which is offset ${newOffset}`
+    );
+    setItemOffset(newOffset);
+  };
+
   return (
     <div className="cardsWrapper">
       <div style={{ textAlign: "right" }}>{props?.data?.length}</div>
-      {props.data?.map((card, index) => {
-        return (
-          <div className="card" key={index}>
-            <p className="date">
-              {card.date} {card.category}
-            </p>
-            <p className="content">
-              <a href={card?.link}>{card.title}</a>
-            </p>
-            <hr />
-
-            <div>
-              {card.linkedThemes?.map((item, i) => (
-                <a href={item.link ? item.link : ""} className="link" key={i}>
-                  {item.name}
-                </a>
-              ))}
-            </div>
-          </div>
-        );
-      })}
+      <Items currentItems={currentItems} />
+      <ReactPaginate
+        nextLabel="next >"
+        onPageChange={handlePageClick}
+        pageRangeDisplayed={3}
+        marginPagesDisplayed={2}
+        pageCount={pageCount}
+        previousLabel="< previous"
+        pageClassName="page-item"
+        pageLinkClassName="page-link"
+        previousClassName="page-item"
+        previousLinkClassName="page-link"
+        nextClassName="page-item"
+        nextLinkClassName="page-link"
+        breakLabel="..."
+        breakClassName="page-item"
+        breakLinkClassName="page-link"
+        containerClassName="pagination"
+        activeClassName="active"
+        renderOnZeroPageCount={null}
+      />
     </div>
   );
 };
--- src/components/FilterPanel/FilterPanel.js
+++ src/components/FilterPanel/FilterPanel.js
@@ -5,6 +5,15 @@ import { useState } from "react";
 import Flatpickr from "react-flatpickr";
 import "flatpickr/dist/flatpickr.css";
 
+import {
+  Accordion,
+  AccordionItem,
+  AccordionItemHeading,
+  AccordionItemButton,
+  AccordionItemPanel,
+} from "react-accessible-accordion";
+import "react-accessible-accordion/dist/fancy-example.css";
+
 const FilterPanel = ({ data, filters, setFilteredData }) => {
   const [theme, setTheme] = useState({ parent: null, id: null });
   const [searchQuery, setSearchQuery] = useState();
@@ -116,96 +125,122 @@ const FilterPanel = ({ data, filters, setFilteredData }) => {
           placeholder="Meklēt pēc atslēgvārda"
           onChange={filterBySearch}
         ></input>
-        {filters?.themes.map((theme) => {
-          return (
-            <ul key={theme.id} role="menu" className="themeFilter">
-              <li
-                role="menuitem"
-                style={{ minWidth: "200px" }}
-                onClick={() => {
-                  setTheme({ parent: true, id: theme.id });
-                  themeHandler({ parent: true, id: theme.id });
-                }}
-                value={theme.id}
-                id={theme.id}
-                key={theme.id}
-              >
-                {theme.title}
-              </li>
-              {theme?.children?.length > 0 &&
-                theme.children.map((child) => {
-                  return (
-                    <ul key={child.id} role="menu" className="themeFilter">
-                      <li
-                        onClick={() => {
-                          setTheme({ parent: false, id: child.id });
-                          themeHandler({ parent: false, id: child.id });
-                        }}
-                        value={child.id}
-                        id={child.id}
-                        key={child.id}
-                      >
-                        {child.title}
-                      </li>
-                    </ul>
-                  );
-                })}
-            </ul>
-          );
-        })}
 
-        {filters?.categories.map((category) => {
-          return (
-            <div key={category.id}>
-              <label htmlFor={category.id} key={category.id}>
-                <input
-                  type="checkbox"
-                  onChange={checkboxHandler}
-                  value={category.id}
-                  id={category.id}
-                  key={category.id}
+        <Accordion allowZeroExpanded style={{ marginTop: "40px" }}>
+          <AccordionItem key={"asda"}>
+            <AccordionItemHeading>
+              <AccordionItemButton>Tiesību jomas</AccordionItemButton>
+            </AccordionItemHeading>
+            <AccordionItemPanel>
+              <Accordion allowZeroExpanded>
+                {filters?.themes.map((theme) => (
+                  <AccordionItem key={theme.id}>
+                    <AccordionItemHeading
+                      onClick={() => {
+                        setTheme({ parent: true, id: theme.id });
+                        themeHandler({ parent: true, id: theme.id });
+                      }}
+                    >
+                      <AccordionItemButton>{theme.title}</AccordionItemButton>
+                    </AccordionItemHeading>
+                    <AccordionItemPanel>
+                      {theme?.children?.length > 0 &&
+                        theme.children.map((child) => {
+                          return (
+                            <ul
+                              key={child.id}
+                              role="menu"
+                              className="themeFilter"
+                            >
+                              <li
+                                onClick={() => {
+                                  setTheme({ parent: false, id: child.id });
+                                  themeHandler({ parent: false, id: child.id });
+                                }}
+                                value={child.id}
+                                id={child.id}
+                                key={child.id}
+                              >
+                                {child.title}
+                              </li>
+                            </ul>
+                          );
+                        })}
+                    </AccordionItemPanel>
+                  </AccordionItem>
+                ))}
+              </Accordion>
+            </AccordionItemPanel>
+          </AccordionItem>
+
+          <AccordionItem key={"asda"}>
+            <AccordionItemHeading>
+              <AccordionItemButton>Satura veids</AccordionItemButton>
+            </AccordionItemHeading>
+            <AccordionItemPanel>
+              {filters?.categories.map((category) => {
+                return (
+                  <div key={category.id}>
+                    <label htmlFor={category.id} key={category.id}>
+                      <input
+                        type="checkbox"
+                        onChange={checkboxHandler}
+                        value={category.id}
+                        id={category.id}
+                        key={category.id}
+                      />
+                      <span>{category.category_name}</span>
+                    </label>
+                  </div>
+                );
+              })}
+            </AccordionItemPanel>
+          </AccordionItem>
+
+          <AccordionItem key={"asda"}>
+            <AccordionItemHeading>
+              <AccordionItemButton>Periods</AccordionItemButton>
+            </AccordionItemHeading>
+            <AccordionItemPanel>
+              <div className="dateSection">
+                <Flatpickr
+                  className="dateInput"
+                  onChange={(date, str, config) => {
+                    setStartDate(config.input.value);
+                  }}
                 />
-                <span>{category.category_name}</span>
-              </label>
-            </div>
-          );
-        })}
-        <div className="dateSection">
-          <Flatpickr
-            className="dateInput"
-            onChange={(date, str, config) => {
-              setStartDate(config.input.value);
-            }}
-          />
-          <p style={{ marginTop: "20px", fontSize: "40px" }}>-</p>
-          <Flatpickr
-            className="dateInput"
-            onChange={(date, str, config) => {
-              setEndDate(config.input.value);
-            }}
-          />
-          <button
-            type="submit"
-            className="searchDateButton"
-            aria-label="button for search"
-            value="Skatīt ziņas pa periodiem"
-            onClick={() => {
-              filterDataByDateHandler();
-            }}
-          >
-            <svg
-              width="14"
-              heigth="14"
-              version="1.1"
-              viewBox="0 0 700 700"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <g>
-                <path d="m624.88 530.12-204.02-204c29.996-35.688 46.422-80.301 46.422-127.48 0-53.059-20.66-102.95-58.184-140.46-37.52-37.512-87.406-58.172-140.46-58.172-53.059 0-102.95 20.66-140.45 58.184-77.441 77.453-77.441 203.46 0 280.92 37.508 37.52 87.395 58.184 140.45 58.184 47.168 0 91.793-16.414 127.48-46.422l204 204zm-471.95-215.77c-63.793-63.805-63.793-167.62 0-231.42 30.906-30.902 71.996-47.926 115.71-47.926 43.715 0 84.805 17.023 115.71 47.926 30.906 30.906 47.926 71.996 47.926 115.71 0 43.715-17.023 84.805-47.926 115.71-30.906 30.906-72.008 47.926-115.71 47.926s-84.805-17.012-115.71-47.926z"></path>
-              </g>
-            </svg>
-          </button>
-        </div>
+                <p style={{ marginTop: "15px", fontSize: "40px" }}>-</p>
+                <Flatpickr
+                  className="dateInput"
+                  onChange={(date, str, config) => {
+                    setEndDate(config.input.value);
+                  }}
+                />
+                <button
+                  type="submit"
+                  className="searchDateButton"
+                  aria-label="button for search"
+                  value="Skatīt ziņas pa periodiem"
+                  onClick={() => {
+                    filterDataByDateHandler();
+                  }}
+                >
+                  <svg
+                    width="14"
+                    heigth="14"
+                    version="1.1"
+                    viewBox="0 0 700 700"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <g>
+                      <path d="m624.88 530.12-204.02-204c29.996-35.688 46.422-80.301 46.422-127.48 0-53.059-20.66-102.95-58.184-140.46-37.52-37.512-87.406-58.172-140.46-58.172-53.059 0-102.95 20.66-140.45 58.184-77.441 77.453-77.441 203.46 0 280.92 37.508 37.52 87.395 58.184 140.45 58.184 47.168 0 91.793-16.414 127.48-46.422l204 204zm-471.95-215.77c-63.793-63.805-63.793-167.62 0-231.42 30.906-30.902 71.996-47.926 115.71-47.926 43.715 0 84.805 17.023 115.71 47.926 30.906 30.906 47.926 71.996 47.926 115.71 0 43.715-17.023 84.805-47.926 115.71-30.906 30.906-72.008 47.926-115.71 47.926s-84.805-17.012-115.71-47.926z"></path>
+                    </g>
+                  </svg>
+                </button>
+              </div>
+            </AccordionItemPanel>
+          </AccordionItem>
+        </Accordion>
       </div>
     </>
   );
